18da9fa85aa2d48d43c9cdf1d1fa29403569ca9a
Zephyr Raine <zephyr.raine@gmail.com>
2022-06-09 10:15:07 +0200

    oui

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-			if ($row['Email'] === $Email && $row['password'] === $password) {
+			if ($row['Email'] === $Email && $row['Password'] === $password) {
 				$_SESSION['id'] = $row['id'];
 				header("Location: home.php");
 				exit();
