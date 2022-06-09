eef6326b937e454b3ec1d645f83f9731f21a903d
CosplayMrPOPO <arthurbaument@free.FR>
2022-06-09 12:08:16 +0200

    je vasi buen merci

---
+++ inscription.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Salut</title>
+</head>
+<body>
+<img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/58ce9765-60a2-4122-80ca-5741769144fa/d4o5qcp-77bbed1d-8591-446c-9458-c1feb592c12c.jpg/v1/fill/w_1024,h_1366,q_75,strp/mr__popo_by_xelikathalx_d4o5qcp-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTM2NiIsInBhdGgiOiJcL2ZcLzU4Y2U5NzY1LTYwYTItNDEyMi04MGNhLTU3NDE3NjkxNDRmYVwvZDRvNXFjcC03N2JiZWQxZC04NTkxLTQ0NmMtOTQ1OC1jMWZlYjU5MmMxMmMuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.kR7eIw7VBJjpdW5f1mKakJRh4tfUMuloMI6ikpt3MD8">
+</body>
+</html>
