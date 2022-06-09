b24590975f4d5476571cdcee692cc0efcd0fa3ac
Farmeno\Timothée Pennec <timothee.pennec4811@gmail.com>
2022-06-09 09:55:03 +0200

    no message

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,13 @@
 <?php 
 
 $sname= "10.96.16.17";
+$port= 3308
 $unmae= "root";
-$password = "root";
+$password = "";
 
 $db_name = "test_db";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $unmae, $password, $db_name, $port);
 
 if (!$conn){
 	echo "Connection failed!";
