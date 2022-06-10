2bf332cbea58bb6dc809950e9884e13637184fcc
CosplayMrPOPO <arthurbaument@free.FR>
2022-06-10 10:18:28 +0200

    debut

--- inscription_page.php
+++ inscription_page.php
@@ -1,25 +1,18 @@
 <?php  
-session_start();
 include "db_conn.php";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-      die("Échec de la connexion : " . mysqli_connect_error());
-}
- 
-echo "Connexion réussie";
- 
-$sql = "INSERT INTO utilisateur (Id, Nom, Prenom, Adresse, Tel, Email, Password) VALUES ('NULL', :Nom);
-if (mysqli_query($conn, $sql)) {
-      echo "Nouveau enregistrement créé avec succès";
-} else {
-      echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_close($conn);
 
+// submit_contact.php
+if (!isset($_POST['email']) || !isset($_POST['message']))
+{
+      echo('Il faut un email et un message pour soumettre le formulaire.');
+    return;
+}     
 
-?>
-
+$email = $_POST['Email'];
+$prenom = $_POST['Prenom'];
+$nom = $_POST['Nom'];
+$adresse = $_POST['Adresse'];
+$tel = $_POST['Tel'];
+$password = $_POST['Password'];
 
