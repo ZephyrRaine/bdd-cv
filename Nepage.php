b22d4027bcd1fbf27046ffab77c8b569c919a19a
Ript0rr <Antoinecraft0@gmail.com>
2022-06-10 10:18:48 +0200

    J'ai fait le nouvelle démarche

---
+++ Nepage.php
@@ -0,0 +1,17 @@
+<p>Texte avant le formulaire</p>
+
+<form method="post" action="Nouvelle-Entreprise.php">
+
+      <p>
+        <label>Nom de l'entreprise</label> : <input type="text" name="Nom" id="Nom"/>
+        <label>Secteur de l'entreprise</label> : <input type="text" name="Secteur" id="Secteur"/>
+        <label>Adresse de l'entreprise</label> : <input type="text" name="Adresse" id="Adresse"/>
+
+        <input type="submit" value="Envoyer" />
+    </p>
+
+   
+</form>
+
+<p>Texte après le formulaire</p>
+
