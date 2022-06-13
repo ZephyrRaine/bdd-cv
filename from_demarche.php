74763b42391959ea13029a165fbc18c5755eaa75
shuutoka <ggchoutca@gmail.com>
2022-06-13 15:59:52 +0200

    ¯\_(ツ)_/¯

--- from_demarche.php
+++ from_demarche.php
@@ -1,19 +1,26 @@
 
 <?php
+
+include "db_conn.php";
+
+$date = $_POST['date'];
+$contact = $_POST['contact'];
+$etat = $_POST['etat'];
+$enterprise = $_POST['enterpise'];
+
 // Ecriture de la requête
 $sqlQuery = 'INSERT INTO demarche(Date_contact, Type_demarche, Etat, Id_contact) VALUES (:Date_contact, :Type_demarche, :Etat, :Id_contact)';
 // Préparation
 $insertRecipe = $conn->prepare($sqlQuery);
 
 $insertRecipe->execute([
-'Date_contact' => $Date_contact,
-'Type_demarche' => $Type_demarche,
-'Etat' => $Etat,
+'Date_contact' => $date,
+'Type_demarche' => $contact,
+'Etat' => $etat,
 'Id_contact' => $Id_contact,
 ]);
 
-	exit();
-	header("Location: nouvelle_demarche.php");
-	exit();
+//	header("Location: nouvelle_demarche.php");
+//	exit();
 
 ?>
--- nouvelle_demarche.php
+++ nouvelle_demarche.php
@@ -4,7 +4,7 @@
 <head> <!-- tête de la page--> 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Google-faut réfléchir les gens</title>
+	<title>Nouvelle démarche</title>
 	<link rel="stylesheet" type="text/css" href="style.css"> <!--link avec le css-->
  
 </head>
@@ -16,8 +16,8 @@
         <p> <label>Selectionnez une date</label> : <input type="date" name="date"/> </p>
 
         <p>
-            <label for="pays">Méthode de contact</label><br />
-            <select name="pays" id="pays">
+            <label for="contact">Méthode de contact</label><br />
+            <select name="contact" id="contact">
                 <option value="">Selectionnez une option</option>
                 <option value="mail">Mail</option>
                 <option value="tel">téléphone</option>
@@ -27,8 +27,8 @@
         </p>
 
         <p>
-            <label for="pays">état de votre demande</label><br />
-            <select name="pays" id="pays">
+            <label for="etat">état de votre demande</label><br />
+            <select name="etat" id="etat">
                 <option value="">Selectionnez une option</option>
                 <option value="cv_send">CV envoyé</option>
                 <option value="await">En attente</option>
@@ -38,8 +38,8 @@
         </p>
 
         <p>
-            <label for="pays">Entreprise</label><br />
-            <select name="pays" id="pays">
+            <label for="enterprise">Entreprise</label><br />
+            <select name="enterprise" id="enterprise">
                 <option value="">Selectionnez une option</option>
                 <option value="1">Entreprise A</option>
                 <option value="2">Entreprise B</option>
