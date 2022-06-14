384dab18f8cd5ee6f56e21f7738b8c4a317bfcb4
Ript0rr <Antoinecraft0@gmail.com>
2022-06-14 15:35:59 +0200

    changement css

--- Nepage.php
+++ Nepage.php
@@ -1,18 +1,45 @@
-<p>Texte avant le formulaire</p>
+<link rel="stylesheet" type="text/css" href="neige.css"> <!--link avec le css-->
 
-<form method="post" action="Nouvelle-Entreprise.php">
 
-      <p>
+
+
+
+</head>
+
+<body>
+
+<div class="login-box">
+  <h2>Nouvelle Démarche</h2>
+  <form method="post" action=Nouvelle-Entreprise.php>
+    <div class="user-box">
+      <div class="user-box">
+    
+    
+                
+    
         <label>Nom de l'entreprise</label> : <input type="text" name="Nom" id="Nom"/>
+  </div>
+
+  <div class="user-box">
         <label>Secteur de l'entreprise</label> : <input type="text" name="Secteur" id="Secteur"/>
+</div>
+
+<div class="user-box">
         <label>Adresse de l'entreprise</label> : <input type="text" name="Adresse" id="Adresse"/>
+</div>
 
-        <input type="submit" value="Envoyer" />
-
-    </p>
-
+                
+      
+   <a>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+     <input type="button" value="Envoyer" />
+    </a>
    
-</form>
+  </form>
+</div>
 
-<p>Texte après le formulaire</p>
+</body>
 
