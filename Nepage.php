a6be01bec7c313f0b004858e9ca6578ebdcea7dc
Ript0rr <Antoinecraft0@gmail.com>
2022-06-14 15:57:16 +0200

    changement css

--- Nepage.php
+++ Nepage.php
@@ -1,21 +1,13 @@
 <link rel="stylesheet" type="text/css" href="neige.css"> <!--link avec le css-->
-
-
-
-
-
 </head>
 
 <body>
 
 <div class="login-box">
-  <h2>Nouvelle Démarche</h2>
+  <h2>Nouvelle-Entreprise</h2>
   <form method="post" action=Nouvelle-Entreprise.php>
     <div class="user-box">
       <div class="user-box">
-    
-    
-                
     
         <label>Nom de l'entreprise</label> : <input type="text" name="Nom" id="Nom"/>
   </div>
@@ -27,15 +19,13 @@
 <div class="user-box">
         <label>Adresse de l'entreprise</label> : <input type="text" name="Adresse" id="Adresse"/>
 </div>
-
-                
-      
+  
    <a>
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-     <input type="button" value="Envoyer" />
+     <input type="submit" value="Envoyer" />
     </a>
    
   </form>
